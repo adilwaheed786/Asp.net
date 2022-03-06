05c980ef312056006434d457ffa5b328c9a153dd
adil waheed <adilwaheed2222.com@gmail.com>
2022-03-06 14:50:09 +0500

    Password Reset Link Code

---
+++ Password_Reset/Password_Reset/Registration/passwordResetLink.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Password_Reset.Registration
+{
+    public partial class passwordResetLink : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spResetPassword", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter paramUsername = new SqlParameter("@UserName", txtUserName.Text);
+                cmd.Parameters.Add(paramUsername);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    if (Convert.ToBoolean(rdr["ReturnCode"]))
+                    {
+                        SendPasswordResetEmail(rdr["Email"].ToString(), txtUserName.Text, rdr["UniqueId"].ToString());
+                        lblMessage.Text = "An email with instructions to reset your password is sent to your registered email";
+                    }
+                    else
+                    {
+                        lblMessage.ForeColor = System.Drawing.Color.Red;
+                        lblMessage.Text = "Username not found!";
+                    }
+                }
+            }
+        }
+        private void SendPasswordResetEmail(string ToEmail, string UserName, string UniqueId)
+        {
+            // MailMessage class is present is System.Net.Mail namespace
+            MailMessage mailMessage = new MailMessage("Rabbiaessani52@gmail.com", ToEmail);
+
+
+            // StringBuilder class is present in System.Text namespace
+            StringBuilder sbEmailBody = new StringBuilder();
+            sbEmailBody.Append("Dear " + UserName + ",<br/><br/>");
+            sbEmailBody.Append("Please click on the following link to reset your password");
+            sbEmailBody.Append("<br/>");
+            sbEmailBody.Append("http://localhost:50365/Registration/ChangedPassword.aspx?uid=" + UniqueId);
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("<b>E-Commerce Testing Purpose</b>");
+       
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Reset Your Password";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "DevelopingPurpose2222@gmail.com",
+                Password = "developingpurpose2222"
+            };
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
