88af21cde18a33c0c3ba0700d32be99287438758
adil waheed <adilwaheed2222.com@gmail.com>
2022-03-13 23:09:31 +0500

    WEB API in MVC

---
+++ WebApi/WebApi/Controllers/ConsumeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        HttpClient client = new HttpClient();
+
+        // GET: Consume
+        public ActionResult Index()
+        {
+            List<Customer> customer = new List<Customer>();
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.GetAsync("CustomerApi");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Customer>>();
+                display.Wait();
+                customer = display.Result;
+            }
+            return View(customer);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer cs)
+        {
+          
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.PostAsJsonAsync<Customer>("CustomerApi", cs);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+            
+        }
+        public ActionResult Details(int id)
+        {
+            Customer customer = null;
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.GetAsync("CustomerApi?id="+id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Customer>();
+                display.Wait();
+                customer = display.Result;
+            }
+            return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Customer customer = null;
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.GetAsync("CustomerApi?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Customer>();
+                display.Wait();
+                customer = display.Result;
+            }
+            return View(customer);
+        }
+        [HttpPut]
+        public ActionResult Edit(Customer cs)
+        {
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.PutAsJsonAsync("CustomerApi", cs);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+     
+        [HttpDelete,ActionName("Delete")]
+        public ActionResult DeleteC(int id)
+        {
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.DeleteAsync("CustomerApi/"+ id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+          
+        }
+        public ActionResult Delete(int id)
+        {
+            Customer customer = null;
+            client.BaseAddress = new Uri("http://localhost:64914/api/CustomerApi");
+            var response = client.GetAsync("CustomerApi?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Customer>();
+                display.Wait();
+                customer = display.Result;
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/CustomerApiController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+namespace WebApi.Controllers
+{
+    public class CustomerApiController : System.Web.Http.ApiController
+    {
+        shoppingv3Entities db = new shoppingv3Entities();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Action()
+        {
+            List<Customer> obj = db.Customers.ToList();
+            return Ok(obj);
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Action(int id)
+        {
+            var obj = db.Customers.Where(model =>model.Cust_id==id).FirstOrDefault();
+            return Ok(obj);
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Insert(Customer ct)
+        {
+            db.Customers.Add(ct);
+            db.SaveChanges();
+            return Ok();
+        }
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult Update(Customer ct)
+        {
+            var obj = db.Customers.Where(model => model.Cust_id == ct.Cust_id).FirstOrDefault();
+            if (ct != null)
+            {
+                obj.Cust_Name = ct.Cust_Name;
+                obj.Cust_id = ct.Cust_id;
+                obj.Address = ct.Address;
+                obj.E_mail = ct.E_mail;
+                obj.Password = ct.Password;
+                obj.Creation_date = ct.Creation_date;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }                    
+            return Ok();
+        }
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult CusDelete(int id)
+        {
+            var obj = db.Customers.Where(model => model.Cust_id == id).FirstOrDefault();
+            db.Entry(obj).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
