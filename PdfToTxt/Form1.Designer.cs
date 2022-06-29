eb499c8e285d248171b2568c483b4d3baea305ba
adil waheed <adilwaheed2222.com@gmail.com>
2022-06-29 14:46:35 +0500

    Add Pdf To Text Convertor

---
+++ PdfToTxt/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PdfToTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convert PDF File In To TEXT File";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload Pdf File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(214, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(45, 258);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(296, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(214, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listBox1.Size = new System.Drawing.Size(296, 121);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(42, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wait To Complete....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF CONVERTOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PdfToTxt/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PdfToTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            progressBar1.Visible = false;
+            label2.Visible = false;
+           
+        }
+        string path;
+        int count = 0;
+        List<string> filePaths;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count = 0;
+                OpenFileDialog op1 = new OpenFileDialog();
+            op1.Multiselect = true;
+                op1.Filter = "allfiles|*.pdf";
+                op1.ShowDialog();
+                string[] FName;
+                filePaths = new List<string>();
+                op1.FileNames.ToList().ForEach(file =>
+                    
+                {
+                        filePaths.Add(file);
+                        listBox1.Items.Add(count+1 +"  "+ file);
+                        count++;
+                }
+
+                );
+                progressBar1.Visible = true;
+                label2.Visible = true;
+               // label2.Text = "Done: 0 of " + count;
+                MessageBox.Show(Convert.ToString(count) + " File(s) copied");
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+           
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                int done = 0;
+              
+                foreach (var path in filePaths)
+                {
+                    SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+                    string dir = Path.Combine(System.IO.Path.GetDirectoryName(Application.ExecutablePath), "ConvertedFiles");
+                    
+                    // If directory does not exist, create it
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                    String filename = Path.GetFileName(path);
+                    filename = Path.ChangeExtension(filename, ".txt");
+                    byte[] pdf = File.ReadAllBytes(path);
+                    f.OpenPdf(pdf);
+                    if (f.PageCount > 0)
+                    {
+                        string text = f.ToText();
+                        // richTextBox1.Text = text;
+                        File.WriteAllText(Path.Combine(dir,filename), text);
+                    
+                    }
+                    done = done+1;
+                    incrementlabel(done);
+ 
+                    incrementProgress(done);
+                }
+                button3.Enabled = true;
+                MessageBox.Show("Successfully Converted "+count+" files");
+              
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Contact To Developer");
+                throw;
+            }
+           
+        }
+
+        public void incrementProgress(int done)
+        {
+            double percent = ((done * 100) / count);
+          //  label2.Text = "Done: " + percent +" of " + count;
+           // Thread.Sleep(30);
+            progressBar1.Value =(int)(Convert.ToInt32(percent));
+            
+
+        }
+        public void incrementlabel(int done)
+        {
+           // MessageBox.Show(" " +done);
+            label2.Text = "Done: " + done + " of " + count;
+            //Thread t = new Thread(new ThreadStart(change));
+            //t.Start();
+            //Thread.Sleep(3000);
+            //t.Abort();
+            
+
+
+        }
+        public void clear()
+        {
+           // richTextBox1.Text = "";
+            path = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            progressBar1.Visible = false;
+            label2.Visible = false;
+            listBox1.Items.Clear();
+            path = "";
+            count = 0;
+            filePaths = new List<string>();
+        }
+    }
+}
